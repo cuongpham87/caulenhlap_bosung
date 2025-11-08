6165adeb2ef3096f28e5233847810e68a7fb0e01
cuongpham87 <lil.steven296@gmail.com>
2025-11-08 23:05:23 +0630

    add
    add

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿//Thực hiện việc in ra các số từ 1 đến 5 bằng cách sử dụng câu lệnh while.
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        int i = 1; 
+        while (i<5)
+        {
+            Console.WriteLine(i);
+            i++; 
+        }
+    }
+}
